f9a367d5d39fb3240f6278fe5bf1b2b4673772e4
Omar Monterrey <omarcoder33@gmail.com>
2021-05-23 20:29:03 -0400

    Messages changes

--- events/cardPlayed.js
+++ events/cardPlayed.js
@@ -1,5 +1,12 @@
 var tableToImage = require('../tableToImage.js');
 function cardPlayed(P, ctx, data){
-	ctx.replyWithPhoto( tableToImage(data.table), {caption: `Carta jugada: <b>${P.getCardName(data.cardPlayed.id)}</b>`, parse_mode:'HTML'} );
+	let placedBy = P.players[ data.cardPlayed.turn ];
+	let nextTurn = P.players[ data.cardPlayed.next ];
+	let cardName = P.getCardName(data.cardPlayed.id);
+	let caption = `Carta jugada por <b>${placedBy.userName}</b>: <b>${cardName}</b>\n`;
+	caption    += `Ahora es turno de <b>${nextTurn.userName}</b>`;
+	ctx.replyWithPhoto( tableToImage(data.table), {caption, parse_mode:'HTML'} ).then(msg => {
+		setTimeout(() => ctx.telegram.editMessageCaption(msg.chat.id,msg.message_id, undefined, `Carta jugada: <b>${cardName}</b>`, {parse_mode: 'HTML'}), 15000);
+	});
 }
 module.exports = cardPlayed;
